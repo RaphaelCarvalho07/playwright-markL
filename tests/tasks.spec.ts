e9a32052186911da3368a4bb912e1620507f759d
Raphael Carvalho <raphaelcarvalho@Raphaels-MacBook-Pro.local>
2024-05-09 15:59:26 -0300

    refactor: create abstraction layers in order to follow the clean code principle. It was created the helper and data model layers

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,35 +1,37 @@
 import { test, expect } from '@playwright/test'
-import exp from 'constants'
+import { TaskModel } from './fixtures/task.model'
+import { deleteTaskByHelper, postTask } from './support/helpers'
 
 test('deve cadastrar uma nova tarefa', async ({ page, request }) => {
-
+    const task: TaskModel = {
+        name: 'Ler um livro de Typescript',
+        is_done: false
+    }
     // Dado que tenho uma nova tarefa
-    const taskName = 'Ler um livro de Typescript'
-    await (request.delete(`http://localhost:3333/helper/tasks/${taskName}`))
+    await deleteTaskByHelper(request, task.name)
 
     // E que estou na página de cadastro
     await page.goto('http://localhost:8080')
 
     // Quando cadastro a nova tarefa
     const InputTaskname = page.locator('input[class*="InputNewTask"]')
-    await InputTaskname.fill(taskName)
+    await InputTaskname.fill(task.name)
 
     await page.click('css=button >> text=Create')
 
     //Então essa tarea deve ser exibida na lista de tarefas
-    const target = page.locator(`css=.task-item p >> text=${taskName}`)
+    const target = page.locator(`css=.task-item p >> text=${task.name}`)
     await expect(target).toBeVisible()
 })
 
-test.only('não deve permitir cadastrar tarefa duplicada', async ({ page, request }) => {
+test('não deve permitir cadastrar tarefa duplicada', async ({ page, request }) => {
 
-    const task = {
+    const task: TaskModel = {
         name: 'Comprar Lego',
         is_done: false
     }
-    await (request.delete(`http://localhost:3333/helper/tasks/${task.name}`))
-    const newTask =await request.post('http://localhost:3333/tasks', { data: task })
-    expect(newTask.ok()).toBeTruthy()
+    await deleteTaskByHelper(request, task.name)
+    await postTask(request, task)
 
     await page.goto('http://localhost:8080')
     const InputTaskName = page.locator('input[class*="InputNewTask"]')
