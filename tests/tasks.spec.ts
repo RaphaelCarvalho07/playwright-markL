736387a9ada3fae5e9f99d42a9ab69622f216e83
Raphael Carvalho <raphaelcarvalho@Raphaels-MacBook-Pro.local>
2024-05-09 16:56:10 -0300

    refactor: create abstraction layers in order to follow the clean code principle. It was created the helper and data model layers

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,27 +1,22 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
+
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, postTask } from './support/helpers'
+import { TasksPage } from './support/pages/tasks/index'
 
 test('deve cadastrar uma nova tarefa', async ({ page, request }) => {
+
     const task: TaskModel = {
         name: 'Ler um livro de Typescript',
         is_done: false
     }
-    // Dado que tenho uma nova tarefa
     await deleteTaskByHelper(request, task.name)
 
-    // E que estou na página de cadastro
-    await page.goto('http://localhost:8080')
+    const tasksPage = new TasksPage(page)
 
-    // Quando cadastro a nova tarefa
-    const InputTaskname = page.locator('input[class*="InputNewTask"]')
-    await InputTaskname.fill(task.name)
-
-    await page.click('css=button >> text=Create')
-
-    //Então essa tarea deve ser exibida na lista de tarefas
-    const target = page.locator(`css=.task-item p >> text=${task.name}`)
-    await expect(target).toBeVisible()
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.shouldHaveText(task.name)
 })
 
 test('não deve permitir cadastrar tarefa duplicada', async ({ page, request }) => {
@@ -33,11 +28,9 @@ test('não deve permitir cadastrar tarefa duplicada', async ({ page, request }) 
     await deleteTaskByHelper(request, task.name)
     await postTask(request, task)
 
-    await page.goto('http://localhost:8080')
-    const InputTaskName = page.locator('input[class*="InputNewTask"]')
-    await InputTaskName.fill(task.name)
-    await page.click('css=button >> text=Create')
+    const tasksPage = new TasksPage(page)
 
-    const target = page.locator('.swal2-html-container')
-    await expect(target).toHaveText('Task already exists!')
+    await tasksPage.go()
+    await tasksPage.create(task)
+    await tasksPage.alertHaveText('Task already exists!')
 })
