3fa91efa0c62ff837731fd89058e08072ef2a99a
Raphael Carvalho <raphaelcarvalho@Raphaels-MacBook-Pro.local>
2024-05-10 16:52:26 -0300

    refactor: add the baseURL on the playwright.config.ts file

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: true,
+    baseURL: 'http://localhost:8080',
   },
 
   /* Configure projects for major browsers */
